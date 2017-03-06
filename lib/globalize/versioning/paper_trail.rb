7c2d98c001ddb19ccf661e1190689c42070f2acb
Robert Pathy <rpathy@sprigghr.com>
2017-03-06 13:08:57 -0500

    Update paper_trail.rb

--- lib/globalize/versioning/paper_trail.rb
+++ lib/globalize/versioning/paper_trail.rb
@@ -23,7 +23,7 @@ ActiveRecord::Base.class_eval do
 end
 
 # to handle different versions of paper_trail
-version_class = PaperTrail::VERSION.is_a?(String) ? Version : PaperTrail::Version
+version_class = PaperTrail::VERSION.is_a?(String) ? PaperTrail::Version : PaperTrail::Version
 
 version_class.class_eval do
 
